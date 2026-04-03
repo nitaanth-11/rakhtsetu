208252f21fb68125e67981bc881b55f4ade38ed9
technoknight700 <nitaanth.panickar@gmail.com>
2026-04-03 21:54:23 +0530

    Deletionns

--- models/request.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class BloodRequest(BaseModel):
-    patient_name: str
-    blood_type: str
-    units: int
-    latitude: float
-    longitude: float
-    urgency: str
